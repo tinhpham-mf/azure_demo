98d106b34e2a57e4a142a85a80cb6c078eca8787
Tinh Pham <91900673+tinhpham-mf@users.noreply.github.com>
2024-10-01 12:58:57 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import multiprocessing
 def main():
     multiprocessing.freeze_support()
     try:
-        uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, workers=5,
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=5,
             # log_config="E:\\Desktop\\backend\\log.ini"
         )
     except Exception as e:
