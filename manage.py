bbee442a7073365a28938c3ab0a9a374800b2498
Tinh Pham <91900673+tinhpham-mf@users.noreply.github.com>
2024-10-01 10:43:36 +0700

    Create manage.py

---
+++ manage.py
@@ -0,0 +1,12 @@
+import uvicorn
+import multiprocessing
+def main():
+    multiprocessing.freeze_support()
+    try:
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=5,
+            # log_config="E:\\Desktop\\backend\\log.ini"
+        )
+    except Exception as e:
+        print(e)
+if __name__ == '__main__':
+    main()
